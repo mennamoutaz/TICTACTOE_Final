f03eb01fce900fd20b979beb20183c162fba54b6
Menna <menna7hoss@gmail.com>
2024-06-15 03:23:14 +0300

    correct4

--- ALL/src/c_time.cpp
+++ ALL/src/c_time.cpp
@@ -8,22 +8,19 @@
 #include <ctime>
 #include <thread>
 #include <map>
-#include <cstdint>
-#include <conio.h>
 #include "c_time.h"
-
 
 using namespace std;
 
-
-
 std::string timeToString(std::chrono::system_clock::time_point timePoint) {
     std::time_t time = std::chrono::system_clock::to_time_t(timePoint);
-    std::tm timeInfo;
-    localtime_s(&timeInfo, &time);
-
     char buffer[TIME_BUFFER_SIZE];
-    std::strftime(buffer, TIME_BUFFER_SIZE, "%Y-%m-%d %H:%M:%S", &timeInfo);
+    std::tm* timeInfo = std::localtime(&time);
 
+    if (timeInfo == nullptr) {
+        return "Error converting time.";
+    }
+
+    std::strftime(buffer, TIME_BUFFER_SIZE, "%Y-%m-%d %H:%M:%S", timeInfo);
     return std::string(buffer);
 }
